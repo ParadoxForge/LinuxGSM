f203a4e11dd81bf74904b8c8689568a615636bd3
Paradox Forge <11194723+ParadoxForge@users.noreply.github.com>
2019-10-18 13:32:06 +0200

    Update check.sh

--- lgsm/functions/check.sh
+++ lgsm/functions/check.sh
@@ -12,6 +12,10 @@ local commandname="CHECK"
 
 check_tmuxception.sh
 
+if [ "${function_selfname}" != "command_monitor.sh" ]; then
+	check_permissions.sh
+fi
+
 if [ "${function_selfname}" != "command_install.sh" ]&&[ "${function_selfname}" != "command_update_functions.sh" ]&&[ "${function_selfname}" != "command_update_linuxgsm.sh" ]&&[ "${function_selfname}" != "command_details.sh" ]&&[ "${function_selfname}" != "command_postdetails.sh" ]; then
 	check_system_dir.sh
 fi
